896a7a451894414f35e72588648d61469fe07eed
fr0stfree <me@frostfree.ru>
2023-03-30 16:04:55 +0300

    remove response model

--- src/auth/router.py
+++ src/auth/router.py
@@ -13,19 +13,20 @@ from .service import User
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
-@router.post('/sign-up', response_model=schemas.UserCreatedResponse, status_code=HTTPStatus.CREATED)
-async def sign_up(user: User = Depends(unique_user)):
+@router.post('/sign-up', status_code=HTTPStatus.CREATED)
+async def sign_up(user: User = Depends(unique_user)) -> schemas.UserCreatedResponse:
     return user
 
 
-@router.post('/sign-in', response_model=schemas.TokenResponse, status_code=HTTPStatus.OK)
-async def sign_in(user: User = Depends(verified_user)):
+@router.post('/sign-in')
+async def sign_in(user: User = Depends(verified_user)) -> schemas.TokenResponse:
     token = create_access_token(user.username)
     return schemas.TokenResponse(access_token=token, token_type="bearer")
 
 
-@router.get('/me', response_model=schemas.UserCreatedResponse, status_code=HTTPStatus.OK)
-async def get_me(username: str = Depends(authentication), session: AsyncSession = Depends(get_session)):
+@router.get('/me')
+async def get_me(username: str = Depends(authentication),
+                 session: AsyncSession = Depends(get_session)) -> schemas.UserCreatedResponse:
     user = await User.get(session, username=username)
     return user
 
