9a1dc579337549afd6c36ce2d7d19004d3d9af45
fr0stfree <me@frostfree.ru>
2023-04-02 20:04:30 +0300

    remove comments

--- src/storage/client.py
+++ src/storage/client.py
@@ -1,19 +1,11 @@
 import boto3
 from botocore.client import BaseClient
 
+from .constants import BUCKET_LOCATION
+
 
 session = boto3.session.Session()
 
 
 def get_client_session() -> BaseClient:
-    return session.client(service_name='s3', endpoint_url='https://storage.yandexcloud.net')
-
-
-# if __name__ == '__main__':
-#     s3.upload_file('Makefile', 'async-bucket-5', 'some-key')
-#
-#     for key in s3.list_objects(Bucket='async-bucket-5')['Contents']:
-#         print(key['Key'])
-#
-#     get_object_response = s3.get_object(Bucket='async-bucket-5', Key='some-key')
-#     print(get_object_response['Body'].read())
+    return session.client(service_name='s3', endpoint_url=BUCKET_LOCATION)
--- src/storage/constants.py
+++ src/storage/constants.py
@@ -4,4 +4,5 @@ from typing import Final
 MIN_FILE_NAME_LENGTH: Final[int] = 3
 MAX_FILE_NAME_LENGTH: Final[int] = 35
 MAX_FILE_SIZE: Final[int] = 1024 * 1024 * 10  # 10 MB
+BUCKET_LOCATION: Final[str] = 'https://storage.yandexcloud.net'
 BUCKET_NAME: Final[str] = 'async-bucket-5'
